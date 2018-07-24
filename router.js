ebeaf3bbb47a4773a9e8494b23c74fded59d1606
shikdernyc <shikdernyc@gmail.com>
2018-07-24 19:09:36 +0000

    Updated routes

--- router.js
+++ router.js
@@ -1,2 +1,37 @@
+require("dotenv").config()
 const express = require("express")
 const router = express.Router()
+const Database = require("./database.js")
+
+const db = new Database(process.env.DB_URL)
+
+router.post('/api/todos/create', (req, res)=>{
+    let {name} = req.body
+    db.addToDo(name)
+    .then((todo)=>{
+        res.send(todo)
+    })
+})
+
+router.post('/api/todos/:id/toggle', (req, res)=>{
+    db.toggleComplete(req.params.id)
+    .then((todo)=>{
+        res.send(todo)
+    })
+})
+
+router.get('/api/todos/:id', (req, res)=>{
+    db.getToDo(req.params.id)
+    .then((todo)=>{
+        res.send(todo)
+    })
+})
+
+router.get('/api/todos', (req, res)=>{
+    db.getAllToDos().
+    then((todos)=>{
+        res.send(todos)
+    })
+})
+
+module.exports = router
