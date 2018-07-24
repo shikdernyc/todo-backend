e7dec3efa4f61fd995bb60af05360c795d32c61d
shikdernyc <shikdernyc@gmail.com>
2018-07-24 19:10:03 +0000

    Express using router.js for '/' routes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 const express = require("express")
 const app = express()
+const router = require("./router.js")
 
 app.listen(process.env.PORT, ()=>{
     console.log("Server running on port " + process.env.PORT)
 })
+
+app.use('/', router)
